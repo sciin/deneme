b13be0be4293c52c8527b11e0a3de8bb469bb2c3
Sefa Cin <sefacin55@gmail.com>
2020-12-19 15:03:40 +0300

    Dialog için sınıf oluşturldu

--- loader.cs
+++ loader.cs
@@ -1,1 +1,46 @@
-Test
+public class DialogAdd
+{
+    private string text;
+    private int id;
+    private int dialogID;
+
+    public DialogAdd(string text, int id, int dialogID)
+    {
+        this.text = text;
+        this.id = id;
+        this.dialogID = dialogID;
+    }
+    public string Text
+    {
+        get
+        {
+            return text;
+        }
+        set
+        {
+            text = value;
+        }
+    }
+    public int Id
+    {
+        get
+        {
+            return id;
+        }
+        set
+        {
+            id = value;         
+        }
+    }
+    public int DialogID
+    {
+        set
+        {
+            dialogID = value;
+        }
+        get
+        {
+            return dialogID;
+        }
+    }
+}
